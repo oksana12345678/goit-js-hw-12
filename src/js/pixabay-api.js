c355665181e1efbf1d920c7bf9affb8c2ff878ea
oksana12345678 <oksana.tanashuk@gmail.com>
2024-03-14 17:35:45 +0100

    fix

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -5,9 +5,18 @@ const baseURL = 'https://pixabay.com/api/';
 
 export async function fetchImages(searchQuery, page, perPage) {
   try {
-    const response = await axios.get(
-      `${baseURL}?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
-    );
+    const response = await axios.get(baseURL, {
+      params: {
+        key: API_KEY,
+        q: searchQuery,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        page: page,
+        per_page: perPage,
+      },
+    });
+
     return response.data.hits;
   } catch (error) {
     console.error('Error fetching images:', error);
